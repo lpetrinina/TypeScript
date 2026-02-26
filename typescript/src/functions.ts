a15fd32334928e9b6d1116634bdb59c66353e95b
Luiza Petrinina <petrinina95@gmail.com>
2026-02-26 11:31:20 +0200

    Function params and return

---
+++ typescript/src/functions.ts
@@ -0,0 +1,39 @@
+// * Function params
+
+function sayHi(name: string) {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi('Peter');
+
+// * Function returns
+
+function calculateDiscount(price: number): number {
+
+    const hasDiscount = true;
+
+    if (hasDiscount) {
+        return price;
+    }
+
+    return price * 0.9;
+}
+
+const finalPrice = calculateDiscount(15);
+console.log(finalPrice)
+
+// ---------------------------------------------------------
+
+const names: string[] = ['Ivan', 'Susan', 'Jane', 'Peter'];
+
+function isNameInList(name: string): boolean {
+    return names.includes(name);
+};
+
+let nameToCheck = 'Jane';
+
+if (isNameInList(nameToCheck)) {
+    console.log(`${nameToCheck} is in the list.`);
+} else {
+    console.log(`${nameToCheck} is not in the list.`);
+}
--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -1,6 +1,7 @@
 import './type-annotations';
 import './union-type';
 import './arrays-and-objects';
+import './functions';
 
 
 
